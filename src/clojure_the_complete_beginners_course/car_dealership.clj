e43024db4e8a7a50f9b605a797ed61718d372092
Oriol Barcelona <obarcelonap@gmail.com>
2020-09-03 11:24:10 +0200

    Improved code readability on getting car prices

--- src/clojure_the_complete_beginners_course/car_dealership.clj
+++ src/clojure_the_complete_beginners_course/car_dealership.clj
@@ -1,15 +1,25 @@
 (ns clojure-the-complete-beginners-course.car-dealership
   (:gen-class))
 
-(defn car-prices
+(def coupons {"SAVE20" 0.2})
+(def cars '(["Fiat" 20000] ["BMW" 60000] ["Ferrari" 100000]))
+
+(defn get-car-prices-by-coupon
+  [coupon]
+  (def discount-factor (get coupons coupon 0))
+  (map
+    (fn [[car price]] [car (- price (* price discount-factor))])
+    cars
+    )
+  )
+
+(defn get-car-prices
   [budget & [coupon]]
-  (def available-cars '(["Fiat" 20000] ["BMW" 60000] ["Ferrari" 100000]))
-  (def discount-factor (if coupon 0.2 0))
-  (filter
-    (fn [[_ price]] (>= budget price))
-    (map
-      (fn [[car price]] [car (- price (* price discount-factor))])
-      available-cars
+  (def available-cars
+    (if coupon
+      (get-car-prices-by-coupon coupon)
+      cars
       )
     )
+  (filter (fn [[_ price]] (>= budget price)) available-cars)
   )
--- test/clojure_the_complete_beginners_course/car_dealership_test.clj
+++ test/clojure_the_complete_beginners_course/car_dealership_test.clj
@@ -6,22 +6,22 @@
   (testing "Car prices"
     (testing "Checks for budget"
       (testing "Can't offer cars when budget lower than min car price"
-        (is (= (count (car-prices 0)) 0))
+        (is (= (count (get-car-prices 0)) 0))
         )
       (testing "Provides cars where price is in the budget"
-        (let [prices (car-prices 80000)]
+        (let [prices (get-car-prices 80000)]
           (is (= (count prices) 2))
           (is (= prices '(["Fiat" 20000] ["BMW" 60000])))
           )
         )
       (testing "Provides cars where price is in the budget after applying the coupon"
-        (let [prices (car-prices 50000 "SAVE20")]
+        (let [prices (get-car-prices 50000 "SAVE20")]
           (is (= (count prices) 2))
           (is (= prices '(["Fiat" 16000.0] ["BMW" 48000.0])))
           )
         )
       (testing "Provides all cars when budget is bigger than max car price"
-        (is (= (count (car-prices 10000000)) 3))
+        (is (= (count (get-car-prices 10000000))))
         )
       )
     )
